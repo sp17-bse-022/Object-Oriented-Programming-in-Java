ef6ba7c4c053feb87524f54f14e5a690a7c7d8d7
Hussa <Hussa@Hamid>
2020-09-27 22:21:20 +0500

    Create info.text

---
+++ Test1/src/test1/Test1.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test1;
+
+/**
+ *
+ * @author Hamid Hussain 
+ */
+public class Test1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print(" I love a programmer\n" +
+"He is always there making codes\n" +
+"On different ways in order\n" +
+"To show how much he loves you so\n" +
+"\n" +
+"There are times when he would\n" +
+"Just throw some complex hints at me\n" +
+"With utmost best I could\n" +
+"Try to find the meaning and see\n" +
+"\n" +
+"See that maybe I'm right\n" +
+"With the theory that I have made\n" +
+"And maybe, just maybe\n" +
+"My words rhyme with what's in your head\n" +
+"\n" +
+"But sometimes I want to\n" +
+"Just let go and then erase it\n" +
+"Sometimes I want you to\n" +
+"Be brave enough to just admit\n" +
+"\n" +
+"That I'm something to you\n" +
+"Not a computer you play with\n" +
+"That your feelings are true\n" +
+"There's no condition that you need\n" +
+"\n" +
+"I am afraid to feel\n" +
+"The tragic end of a sonnet\n" +
+"Where two lovers for real\n" +
+"Are mere strangers who'll never ever meet");
+    }
+    
+}
